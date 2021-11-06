009324d4e0e893c59d7b55eb027e759bd68f2000
justis800310 <justis_800310@hotmail.com>
2021-11-06 17:13:02 -0600

    Update pq-linklist.c
    insertion sort, each node sort when enter. Done in Windows, need test in linux. Logic should be correct?

--- pq-linklist.c
+++ pq-linklist.c
@@ -23,10 +23,37 @@ pq* pq_create()
 /* Adds value to pq based on numerical order of key */
 void pq_push(pq *head, double key, void *value)
 {
-  	head->key=key;
-  	head->value=value;
-  	head->next = head_n;
-  	head_n = head;	
+	head->key=key;
+  	head->value=&value;
+  	//if list is empty
+  	if(head_n==NULL){
+  		head_n = head;
+  		head_n->next = NULL;
+	}else{
+	//if something in the list
+		struct pq *current = head_n;
+		struct pq *temp = NULL;
+		//finding the place (small -> large) till current=NULL
+	    while((head->value > current->value) && current!=NULL) {
+	    	temp = current; //tracking old node for replacing
+	    	current = current->next;
+	    	}
+		//push value is the largest in the list
+		if(current == NULL){
+			current = head;
+			current->next = NULL;
+		}else if(current==head_n){
+		//push value is the smallest in the list
+			head->next=current;	//new node become the head
+			head_n=head;	
+		}else{
+		//something between the list
+			head->next = current;
+			temp->next = head;	
+		}
+		
+	}
+		
   //if (head && !(head->ptr)) head->ptr = value;
 }
 
@@ -45,3 +72,4 @@ void pq_destroy(pq *head)
   free(head);
 }
 
+//just for complie void main(){}
